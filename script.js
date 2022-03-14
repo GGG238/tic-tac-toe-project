0db9adc1ea3fd1c39bd8b5508c3c31ebfe94b8a7
Gilberto García <GGG12022021@gmail.com>
2022-03-13 21:02:21 -0600

    Add functions

--- script.js
+++ script.js
@@ -1,26 +1,78 @@
-const gameboard = () => {
+const gb = document.getElementById("gameboard");
 
-}
+const gameboard = (() => {
+    const create = (gBoard) => {
+        for(let i=0; i<9; i++){
+            let div = document.createElement("div");
+            div.dataset.num = i;
+            div.addEventListener("click", disp);
+            gBoard.appendChild(div);
+        }
+    };
 
-const displayController = () => {
-    
-}
-
-const player = (() => {
-
+    return {
+        create,
+    };
 })();
 
+const displayController = (() => {
+    const game = [,,,,,,,,,];
+    let currentSymbol = "X";
+    let symbolsCounter = 0;
+
+    const displaySymbol = (square) => {
+        let num = square.dataset.num;
+        if(game[num]) return;
+
+        game[num] = currentSymbol;
+        square.textContent = currentSymbol;
+        currentSymbol = currentSymbol === "X" ? "O" : "X";
+        symbolsCounter++;
+        
+        if(symbolsCounter > 4){
+            checkWinner();
+        }
+    }
+
+    const checkWinner = () => {
+        const arrX = [];
+        const arrO = [];
+
+        for(let i=0 ; i<game.length ; i++){
+            if(!game[i]) continue;
+            if(game[i]==="X"){
+                arrX.push(i);
+            }else{
+                arrO.push(i);
+            }
+        }
+
+        let regex = /(012||345||678||034||147||258||048||246)/g;
+
+        let strX = arrX.join("");
+        let strO = arrO.join("");
+
+        if(regex.test(strX)){
+            console.log("X won!");
+        }else if(regex.test(strO)){
+            console.log("O won!");
+        }
+        console.log(arrX, arrO);
+    }
+
+    return {
+        displaySymbol,
+    }
+})();
+
+const player = () => {
+
+};
 
 
-const game = ["", "", "", "", "", "", "", "", ""];
-const gb = document.getElementById("gameboard");
-let count = 0;
 
-for(let sym of game){
-    let div = document.createElement("div");
-    div.textContent = sym;
-    div.dataset.num = count;
-    div.addEventListener("click", displayController.abc);
-    count ++;
-    gb.appendChild(div);
+gameboard.create(gb);
+
+function disp(){
+    displayController.displaySymbol(this);
 }
